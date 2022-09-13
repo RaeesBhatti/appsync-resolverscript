dd5138e7aece14a52fd20b09aeeec377f2162061
Raees Iqbal <10067728+RaeesBhatti@users.noreply.github.com>
2022-09-12 22:25:11 -0400

    Add Time util

--- src/AppSyncUtil.ts
+++ src/AppSyncUtil.ts
@@ -1,12 +1,14 @@
 import { stringify } from './utils/stringify'
 import { vtl, VelocityFragment } from './VelocityFragment'
 import { DynamoDB } from './DynamoDB'
-import { VelocityObject, VelocityString, VoidType } from './velocity-types'
+import { VelocityBoolean, VelocityLong, VelocityObject, VelocityString, VoidType } from './velocity-types'
 
 export class AppSyncUtil {
   dynamodb = new DynamoDB()
 
   str = new Str()
+
+  time = new Time()
 
   isList = (value: VelocityObject): VelocityFragment<boolean> => vtl`$util.isList(${stringify(value)})`
 
@@ -21,7 +23,46 @@ export class AppSyncUtil {
 
   unauthorized = (): VelocityFragment<VoidType> => vtl`$util.unauthorized()`
 
-  toJson = (value: VelocityObject): VelocityFragment<string> => vtl`$util.toJson(${stringify(value)})`
+  toJson = (value: VelocityObject | VelocityLong | VelocityBoolean): VelocityFragment<string> =>
+    vtl`$util.toJson(${stringify(value)})`
+}
+
+export class Time {
+  nowISO8601 = (): VelocityFragment => vtl`$util.time.nowISO8601()`
+  nowEpochSeconds = (): VelocityFragment => vtl`$util.time.nowEpochSeconds()`
+  nowEpochMilliSeconds = (): VelocityFragment => vtl`$util.time.nowEpochMilliSeconds()`
+  nowFormatted = (str: VelocityString): VelocityFragment => vtl`$util.time.nowFormatted(${stringify(str)})`
+  nowFormattedWithTimeZone = (fmt: VelocityString, timeZone: VelocityString): VelocityFragment =>
+    vtl`$util.time.nowFormatted(${stringify(fmt)}, ${stringify(timeZone)})`
+
+  parseFormattedToEpochMilliSeconds = (time: VelocityString, fmt: VelocityString): VelocityFragment =>
+    vtl`$util.time.parseFormattedToEpochMilliSeconds(${stringify(time)}, ${stringify(fmt)})`
+
+  parseFormattedToEpochMilliSecondsWithTimezone = (
+    time: VelocityString,
+    fmt: VelocityString,
+    timeZone: VelocityString
+  ): VelocityFragment =>
+    vtl`$util.time.parseFormattedToEpochMilliSeconds(${stringify(time)}, ${stringify(fmt)}, ${stringify(timeZone)})`
+
+  parseISO8601ToEpochMilliSeconds = (time: VelocityString): VelocityFragment =>
+    vtl`$util.time.parseISO8601ToEpochMilliSeconds(${stringify(time)})`
+
+  epochMilliSecondsToSeconds = (time: VelocityLong): VelocityFragment =>
+    vtl`$util.time.epochMilliSecondsToSeconds(${stringify(time)})`
+
+  epochMilliSecondsToISO8601 = (time: VelocityLong): VelocityFragment =>
+    vtl`$util.time.epochMilliSecondsToISO8601(${stringify(time)})`
+
+  epochMilliSecondsToFormatted = (time: VelocityLong, fmt: VelocityString): VelocityFragment =>
+    vtl`$util.time.epochMilliSecondsToFormatted(${stringify(time)}, ${stringify(fmt)})`
+
+  epochMilliSecondsToFormattedWithTimezone = (
+    time: VelocityLong,
+    fmt: VelocityString,
+    timezone: VelocityString
+  ): VelocityFragment =>
+    vtl`$util.time.epochMilliSecondsToFormatted(${stringify(time)}, ${stringify(fmt)}, ${stringify(timezone)})`
 }
 
 export class Str {
